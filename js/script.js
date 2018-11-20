638f35e5156dd8ffe5f8850e0e2fd80981b074b0
osdal <osdal67@gmail.com>
2018-11-21 00:15:41 +0200

    1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-﻿$('#autocomplete').autocomplete({
-    serviceUrl: '/autocomplete/countries',
-    onSelect: function (suggestion) {
-        alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
-    }
-});
+﻿var base = ["vghkf", "dthdt", "gigi"];
+
+var suggestion = $('<div></div>'),
+    inputData = $("#autocomplete").val();
+
+    console.log($("#autocomplete").val());
+
+$('<div></div>').text(inputData);
+
+$("#autocomplete").after(suggestion);
+
+$( "input" )
+  .keyup(function() {
+    var value = $( this ).val();
+    $( "p" ).text( value );
+  })
+  .keyup();
