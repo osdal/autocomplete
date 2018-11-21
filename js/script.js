483839bf2488b49a4e7b94d34b4d85f83b2cf53b
osdal <osdal67@gmail.com>
2018-11-21 17:06:16 +0200

    Промежуточный

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
-﻿var base = ["vghkf", "dthdt", "gigi"];
+﻿var base = ["aaabbb", "aabcdc", "aaaddd", "e", "ea"],
+	inp =$('#autocomplete').val(),
+	res,
+	listRes,
+	suggestion = $('<div></div>');
 
-var suggestion = $('<div></div>');
+
 
 
 $('#autocomplete').keyup(function() {
-			suggestion.text($('#autocomplete').val())
+			 inp = $('#autocomplete').val();
+			
+			 for (let i= 0; i < base.length; i++) {
+					if(base[i].indexOf(inp) != -1 && inp != '') {
+						console.log(base[i]);
+						res = $("<p>" + base[i] + "</p>");
+						listRes = $("#autocomplete").after(res);
+
+					} else if () {
+
+					}
+			}
+			
 		});
 
 
-$("#autocomplete").after(suggestion);
+	
+
+
+
+
 
