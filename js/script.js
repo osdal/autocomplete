ef74600e0f706fe552402e3c122409ddeec5c202
osdal <osdal67@gmail.com>
2018-11-21 23:39:31 +0200

    1

--- js/script.js
+++ js/script.js
@@ -1,29 +1,24 @@
-﻿var base = ["aaabbb", "aabcdc", "aaaddd", "e", "ea"],
-	inp =$('#autocomplete').val(),
-	res,
-	listRes,
-	suggestion = $('<div></div>');
-
-
-
-
-$('#autocomplete').keyup(function() {
-			 inp = $('#autocomplete').val();
-			
-			 for (let i= 0; i < base.length; i++) {
-					if(base[i].indexOf(inp) != -1 && inp != '') {
-						console.log(base[i]);
-						res = $("<p>" + base[i] + "</p>");
-						listRes = $("#autocomplete").after(res);
-
-					} else if () {
-
-					}
-			}
-			
-		});
-
-
+﻿$(function autocomplete(inp, arr) {
+	/*функция принимает два аргумента,
+  значеие поля ввода и массив возможных значений*/
+  var currentFocus;
+  /*функция запускается, когда ничмнают что-либо вводить в поле ввода*/
+  $('#myInput').on('input', function() {
+  	var a, b, i, val = this.value; /*получаем значение поля ввода*/
+  	
+      closeAllLists();/*функция, которая убирает все открытые поля автодополнения кроме того, котрое указано в аргументе*/
+      function closeAllLists(elem) {
+	    var x = $(".autocomplete-items");
+	    for (let i = 0; i < x.length; i++) {
+	      if (elem != x[i] && elem != inp) {
+	        x[i].parent().children(x[i]).remove();
+	      }
+	    }
+	  }
+  	 console.log(val);
+  });
+ 
+})
 	
 
 
